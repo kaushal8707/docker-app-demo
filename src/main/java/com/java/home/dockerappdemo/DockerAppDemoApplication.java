d293bbabb99b5935997201701ce738e1d47be22d
Kaushal Kumar <kaushal_kumar1@persistent.com>
2024-01-31 15:49:45 +0530

    first commit

---
+++ src/main/java/com/java/home/dockerappdemo/DockerAppDemoApplication.java
@@ -0,0 +1,23 @@
+package com.java.home.dockerappdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+@RequestMapping("/service")
+public class DockerAppDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerAppDemoApplication.class, args);
+	}
+
+	@GetMapping("/show")
+	public String publish(){
+		return "my first published application......!!!";
+	}
+
+}
